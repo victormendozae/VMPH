2149b441d9ce2e940f2335e51bc9e8c2d43cf91d
Victor Mendoza <vmmendozae@unal.edu.co>
2020-06-22 00:43:58 -0500

    animacion galeria con JS

--- js/train.js
+++ js/train.js
@@ -4,6 +4,7 @@ function startTime() {
     var m = today.getMinutes();
     var s = today.getSeconds();
     var y = today.getFullYear();
+    h = check(h);
     m = checkTime(m);
     s = checkTime(s);
     document.getElementById('txt').innerHTML =
@@ -15,4 +16,16 @@ function startTime() {
   function checkTime(i) {
     if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
     return i;
+  }
+  function check(i) {
+    if (i >= 12) {i ="0" + (i - 12)};
+    return i;
+  }
+  function resta() {
+    var n = document.getElementById('res').textContent;
+    document.getElementById('res').innerHTML = (n-1);
+  }
+  function suma() {
+    var n = parseInt(document.getElementById('res').textContent, 10);
+    document.getElementById('res').innerHTML = (n+1);
   }
